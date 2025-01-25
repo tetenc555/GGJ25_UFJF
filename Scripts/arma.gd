63854ad0acf0bd60fba79ae292f99a13c0303148
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-01-25 14:55:33 -0300

    Change gun script

--- Scripts/arma.gd
+++ Scripts/arma.gd
@@ -2,28 +2,19 @@ extends Node2D
 
 
 @export var projectile_scene: PackedScene = preload("res://Scenes/Projectile.tscn")  # Path to your projectile scene
+@export var projectile_speed = 0
 
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):  # Check if the fire button is pressed
 		shoot_projectile()
+	rotation = 180
 
 func shoot_projectile():
-	# Instantiate the projectile from the PackedScene
 	var projectile_instance = projectile_scene.instantiate()
-	# Sets the default direction being down so it matches the sprite position
-	var direction := Vector2(0,1)
-	# If any directional input is pressed it changes the direction to that input to match the sprite
-	if Input.get_vector("AimLeft","AimRight","AimNorth","AimSouth"):
-		direction = Input.get_vector("AimLeft","AimRight","AimNorth","AimSouth")
-	print(direction)	
-	# Set the initial position of the projectile in front of the player
-	projectile_instance.position = get_parent().position + direction  # Example offset
-
-	# Set the direction of the projectile
+	var direction = global_position.direction_to(get_global_mouse_position())
+	projectile_instance.position = get_parent().position + direction
 	projectile_instance.set_direction(direction) 
 
-	# Set the speed of the projectile (this is a custom variable defined in the projectile script)
-	projectile_instance.speed = 400  # Set speed properly here (this value has priority over the projectile script )
-
-	# Add the projectile to the scene
+	projectile_instance.speed = projectile_speed
+	
 	get_parent().add_child(projectile_instance)
