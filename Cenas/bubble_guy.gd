6319ad6c024bb6b03367b9948b289717b727bfc1
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-01-24 23:37:34 -0300

    metodos dbasicoes de vida para o personagem

--- Cenas/bubble_guy.gd
+++ Cenas/bubble_guy.gd
@@ -2,7 +2,22 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
+var life = 100.0
 
+func _ready():
+	life = 100.0
+
+func decrease_life(amount):
+	if (amount>0):
+		life-=amount;
+	if (life <0):
+		life = 0;
+	
+func increase_life(amount):
+	if (amount>0 || life<100):
+		life+=amount;
+	if (life>100):
+		life=100
 
 func _physics_process(delta):
 	var direction := Input.get_vector("Left","Right","Up","Down")
