693683af06b7058c405a0186e820557bf5f4dee0
Bebs Fiorillo <estevaovrb2014@gmail.com>
2025-01-25 02:24:02 -0300

    spawn of enemy is properly implemented (alpha)

---
+++ Cenas/game.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var spawnInimigoTimer = $spawnInimigo
+@onready var player = $BubbleGuy
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawnInimigoTimer.start() #spawns enemy in 5 seconds! Please change this as enemys should be called. this is only to be generic and alpha testing purpouses.
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_spawn_inimigo_timeout() -> void: #maybe the enemy should be passed as parameter. This should be more generic
+	var new_enemy = preload("res://Cenas/inmigodasilva.tscn").instantiate()
+	new_enemy.global_position = Vector2(-50,50) #positionStart + Vector2(-20,20)
+	if (new_enemy != null):
+		add_child(new_enemy)
+		var positionStart= player.global_position
+	pass # Replace with function body.
