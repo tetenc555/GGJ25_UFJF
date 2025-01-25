318a023ce7a980a2c65a58e39e0f680402de068c
João Srbek <joao.srbek@estudante.ufjf.br>
2025-01-24 23:20:27 -0300

    Movimento protótipo pronto

--- Cenas/bubble_guy.gd
+++ Cenas/bubble_guy.gd
@@ -2,23 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_to
+	var direction := Input.get_vector("Left","Right","Up","Down")
+	velocity = SPEED * direction.normalized()
+		
 	move_and_slide()
