3e53b38526e90adc038c395199955919b77117f0
João Srbek <joao.srbek@estudante.ufjf.br>
2025-01-26 01:14:30 -0300

    Create enemy movement (prototype)

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,12 +2,28 @@ extends CharacterBody2D
 
 # Enemy health
 @export var max_health: int = 100
+@onready var raycastLeft: RayCast2D = $RayCastLeft
+@onready var raycastRight: RayCast2D = $RayCastRight
+var direction = 1
 var current_health: int
+const SPEED = 200.0
 
 func _ready() -> void:
 	# Initialize health
 	current_health = max_health
 
+func _physics_process(delta):
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if raycastRight.is_colliding():
+		direction = -1
+	if raycastLeft.is_colliding():
+		direction = 1
+	
+	move_and_slide()
 # Method to take damage
 func take_damage(amount: int) -> void:
 	current_health -= amount
