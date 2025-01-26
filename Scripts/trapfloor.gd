c69f930fb6ad3f58054e12b888aa5447f943c489
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2025-01-26 14:45:04 -0300

    Add trapfloor

--- Scripts/trapfloor.gd
+++ Scripts/trapfloor.gd
@@ -2,27 +2,28 @@ extends Area2D
 
 var cooldown = false
 var body_in_area: CharacterBody2D = null
-@onready var cooldownTimer = $CoolDown
+@onready var cooldownTimer = $Cooldown
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if body_in_area == null:  
-		body_in_area = body  #defines player body
+		body_in_area = body
 		if not cooldown:
+			cooldownTimer.start()
 			apply_damage()
 
 func _on_body_exited(body: CharacterBody2D) -> void:
 	if body == body_in_area:
 		body_in_area = null  
-		cooldownTimer.stop()  # Stop the cooldown timer
 
 func _on_cool_down_timeout() -> void:
 	cooldown = false
-	if body_in_area != null:  # If the body still in the area it takes damage
+	if body_in_area != null: 
 		apply_damage()
+		cooldownTimer.start()
 
 
 func apply_damage() -> void:
+	sprite.play("damage")
 	if body_in_area != null:
-		body_in_area.decrease_life(20)  # Reduce life by 20
-		cooldown = true
-		cooldownTimer.start()
+		body_in_area.decrease_life(20)
