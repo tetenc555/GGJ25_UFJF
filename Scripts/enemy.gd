a5f8021ddaa63a812217c9143a1fe746e206f0bc
João Srbek <joao.srbek@estudante.ufjf.br>
2025-01-26 02:15:20 -0300

    Fix enemy movement

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,26 +2,27 @@ extends CharacterBody2D
 
 # Enemy health
 @export var max_health: int = 100
-@onready var raycastLeft: RayCast2D = $RayCastLeft
-@onready var raycastRight: RayCast2D = $RayCastRight
-var direction = 1
+@onready var timer = $Timer
+@export var SPEED = 100.0
+@export var movement_type : String
+@export var direction := Vector2i(0,0)
 var current_health: int
-const SPEED = 200.0
 
 func _ready() -> void:
 	# Initialize health
 	current_health = max_health
 
 func _physics_process(delta):
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if raycastRight.is_colliding():
-		direction = -1
-	if raycastLeft.is_colliding():
-		direction = 1
+	if movement_type == "H":
+		if direction.x:
+			velocity.x = direction.x * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	elif movement_type == "V":
+		if direction.y:
+			velocity.y = direction.y * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 	move_and_slide()
 # Method to take damage
@@ -38,3 +39,7 @@ func die() -> void:
 	print("Enemy has died!")
 	# Add death logic here, e.g., play animation, emit signal, or remove from scene
 	queue_free()
+
+
+func _on_timer_timeout():
+	direction = -direction
