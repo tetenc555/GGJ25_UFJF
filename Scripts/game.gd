637f401f858293ba3475ce6e600256abb3117f42
Liz <elizabeth.admirable@gmail.com>
2025-01-26 21:58:07 +0000

    balance wasp, autospawn

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var spawner = $Spawner
 @onready var player = $Player
 
-@export var spawn_radius: float = 10
+@export var spawn_radius: float = 40
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#spawner.start() #spawns enemy in 5 seconds! Please change this as enemys should be called. this is only to be generic and alpha testing purpouses.
@@ -15,16 +15,26 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_spawn_inimigo_timeout() -> void: #maybe the enemy should be passed as parameter. This should be more generic
-	var new_enemy = preload("res://Scenes/Enemies/Hedgehog/hedgehog.tscn").instantiate()
+	var hedgehog = preload("res://Scenes/Enemies/Hedgehog/hedgehog.tscn").instantiate()
+	var wasp = preload("res://Scenes/Enemies/Hedgehog/wasp.tscn").instantiate()
 	var spawnPos = get_spawn_pos()
-
+	var randomizer = randi_range(0,1)
+	var new_enemy
+	if randomizer == 1:
+		new_enemy = hedgehog
+	elif randomizer == 0:
+		new_enemy = wasp
 	if (new_enemy != null && is_valid_spawn(spawnPos)):
 		add_child(new_enemy)
 		new_enemy.global_position = spawnPos
 		var _positionStart = player.global_position
 	pass # Replace with function body.
 func get_spawn_pos():
-		return Vector2(randf_range(-100,100), randf_range(70,-90))
+		return Vector2(randf_range(-260,260), randf_range(160,-160))
 func is_valid_spawn(spawnPos: Vector2) -> bool:
 	var distance_to_player = player.position.distance_to(spawnPos)
 	return distance_to_player > spawn_radius
+
+
+func _on_game_time_timeout() -> void:
+	pass
